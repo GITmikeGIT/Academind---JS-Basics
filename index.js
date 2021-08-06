e5e3a6466d3658d2edb9a3df32362c5215f55f90
GITmikeGIT <michalzajczyk@gmail.com>
2021-08-06 10:38:48 +0200

    Data types

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var vars = [true, 'text', 5, 5.5];
+vars.forEach(value => {
+  console.log([value, typeof value]);
+});
+console.log(typeof vars);
