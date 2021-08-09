5140c09f5c34ee42e8bc0ca880be3b491fbffe43
GITmikeGIT <michalzajczyk@gmail.com>
2021-08-09 14:41:33 +0200

    Operators - Multiplication & Floating problem

--- Lessons/operators.js
+++ Lessons/operators.js
@@ -95,3 +95,34 @@ console.log(a - b);
 a = 12;
 b = NaN;
 console.log(a - b);
+
+/** Multiplication & Floating point problems */
+
+a = 12;
+b = 2;
+
+a *= b;
+console.log(a);
+
+a = 1.3;
+b = 2.2;
+
+console.log(a * b);
+
+if ((a * b).toFixed(2) == 2.86) console.log('true');
+else console.log('false');
+
+a = 2;
+b = '2.2';
+
+console.log(a * b);
+
+a = 2;
+b = null;
+
+console.log(a * b);
+
+a = 2;
+b = Infinity;
+
+console.log(a * b);
