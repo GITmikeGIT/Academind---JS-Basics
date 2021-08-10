774b77ff56c01660383a2155008f998da503b480
GITmikeGIT <michalzajczyk@gmail.com>
2021-08-10 12:39:13 +0200

    1 file modified

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 // Import Javascripts
-import './Lessons/variable-types.js';
-import './Lessons/strict-mode.js';
-import './Lessons/dynamic-typing.js';
-import './Lessons/hoisting.js';
-import './Lessons/functions.js';
-import './Lessons/control-structures.js';
-import './Lessons/operators.js';
+import './Lessons/Basics/variable-types.js';
+import './Lessons/Basics/strict-mode.js';
+import './Lessons/Basics/dynamic-typing.js';
+import './Lessons/Basics/hoisting.js';
+import './Lessons/Basics/functions.js';
+import './Lessons/Basics/control-structures.js';
+import './Lessons/Basics/operators.js';
