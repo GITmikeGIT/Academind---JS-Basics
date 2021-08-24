33e20189b048eabc6c79464c22524fbbda73bb95
GITmikeGIT <michalzajczyk@gmail.com>
2021-08-24 11:14:29 +0200

    Functions - IIFE

--- Lessons/Functions/closure.js
+++ Lessons/Functions/closure.js
@@ -13,3 +13,26 @@ var calc = generator(900);
 var calcAnother = generator(100);
 console.log(calc());
 console.log(calcAnother());
+
+/** Immediately Invoked Function Executions (IIFEs) */
+console.log('Immediately Invoked Function Executions (IIFEs)');
+/**
+ * To not pollute global variable values - creates local scope
+ */
+(function calc() {
+  var number = 10;
+  console.log(number);
+})();
+
+(function calcWithParam(input) {
+  var number = input ?? 0;
+  console.log(number);
+})(100);
+
+var obj = {};
+/** Overwriting global variable inside IIFE function */
+(function overwrite(input) {
+  obj.name = 'Max';
+})(obj);
+
+console.log(obj);
